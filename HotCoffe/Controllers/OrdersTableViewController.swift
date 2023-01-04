0ae13b2067d3c0dd844dfe6d71f2bf8393ef0c73
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-01-03 20:28:23 -0600

    21 Implementing ViewModels

--- HotCoffe/Controllers/OrdersTableViewController.swift
+++ HotCoffe/Controllers/OrdersTableViewController.swift
@@ -9,5 +9,32 @@ import Foundation
 import UIKit
 
 class OrdersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        populateOrders()
+    }
     
+    private func populateOrders() {
+        
+        guard let coffeeOrdersURL = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else {
+            fatalError("URL was incorrect")
+        }
+        
+        let resource = Resource<[Order]>(url: coffeeOrdersURL)
+        
+        Webservice().load(resource: resource) { result in
+            
+            switch result {
+                case .success(let orders):
+                    print(orders)
+                print("Debug: Orders: ")
+                case .failure(let error):
+                    print(error)
+            }
+            
+        }
+        
+    }
 }
---
+++ HotCoffe/View Models/OrderViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  OrderViewModel.swift
+//  HotCoffe
+//
+//  Created by Marco Alonso Rodriguez on 03/01/23.
+//
+
+import Foundation
+
+//will provide de data for TableView
+class OrderListViewModel {
+    
+    var ordersViewModel: [OrderViewModel]
+    
+    init() {
+        //empty array
+        self.ordersViewModel = [OrderViewModel]()
+    }
+}
+
+extension OrderListViewModel {
+    func orderViewModel(at index : Int) -> OrderViewModel {
+        return self.ordersViewModel[index]
+    }
+}
+
+struct OrderViewModel {
+    let order: Order
+}
+
+extension OrderViewModel {
+    
+    var name: String {
+        return self.order.name
+    }
+    
+    var email: String {
+        return self.order.email
+    }
+    
+    var type: String {
+        return self.order.type.rawValue.capitalized
+    }
+    
+    var size: String {
+        return self.order.size.rawValue.capitalized
+    }
+}
